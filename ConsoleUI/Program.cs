9a0680403ec8b74cf78ae960ca46b10f77b3746b
FeritYILDIZ <feritstar@gmail.com>
2023-06-10 22:56:28 +0300

    Entity Framework, Sql Server, Local Database is added.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 using System;
 
@@ -8,12 +9,30 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            CarManager carManager = new CarManager(new InMemoryCarDal());
+            //CarManager carManager = new CarManager(new InMemoryCarDal());
 
-            foreach (var item in carManager.GetAll())
+            //foreach (var item in carManager.GetAll())
+            //{
+            //    Console.WriteLine(item.ModelYear + " " + item.Description);
+            //}
+
+            CarManager carManager = new CarManager(new EfCarDal());
+
+            //foreach (var car in carManager.GetAll())
+            //{
+            //    Console.WriteLine(car.Description);
+            //}
+
+            //foreach (var car in carManager.GetCarsByBrandId(1))
+            //{
+            //    Console.WriteLine(car.Description);
+            //}
+
+            foreach (var car in carManager.GetCarsByColorId(2))
             {
-                Console.WriteLine(item.ModelYear + " " + item.Description);
+                Console.WriteLine(car.Description);
             }
         }
+
     }
 }
---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    /// <summary>
+    /// T must be a reference type plus T should be IEntity or an object which is implemented IEntity
+    /// class : reference type filter
+    /// IEntity: T should be IEntity or an object which is implemented IEntity
+    /// new(): should be an object which can be created by "new".
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IEntityRepository<T> where T:class, IEntity, new()
+    {
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
